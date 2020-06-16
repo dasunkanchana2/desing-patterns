143f8aecc83b20a1ac4e9436fa6ed0dd840477ba
Dasun <dasun@zmessenger.com>
2020-06-16 18:18:27 +0530

    #Added - design-pattern project

---
+++ src/main/java/com/dasun/desingpattern/App.java
@@ -0,0 +1,13 @@
+package com.dasun.desingpattern;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
